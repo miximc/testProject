db8dc791049710e8341cab7aafc6cd6b4bff5528
mixi <miximc@mail.ru>
2022-12-08 22:04:58 +0300

    Добавил один принт

---
+++ KozlovREPOZ.py
@@ -0,0 +1,15 @@
+time = 25
+while time > 24:
+
+    time = int(input('Сколько сейчас часов? '))
+    if time > 24:
+        print('Вы ввели неправильный час, повторите.')
+if 6 < time < 12:
+    print('Доброе утро!')
+elif 11 < time < 17:
+    print('Добрый день!')
+elif 16 < time < 24:
+    print('Добрый вечер!')
+else:
+    print('Доброй ночи!')
+
